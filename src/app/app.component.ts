f0732a0fd0e1fa6029ac4265769a5c6832991a4e
danielwiehl <daniel.wiehl@yahoo.com>
2024-07-04 08:20:56 +0200

    install SCION Workbench

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {WorkbenchComponent} from '@scion/workbench';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    WorkbenchComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'bison-workbench-app';
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
 import {ApplicationConfig} from '@angular/core';
+import {provideWorkbench} from '@scion/workbench';
 import {provideRouter} from '@angular/router';
+import {provideAnimations} from '@angular/platform-browser/animations';
 import {routes} from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
+    provideAnimations(),
+    provideWorkbench(),
   ],
 };
